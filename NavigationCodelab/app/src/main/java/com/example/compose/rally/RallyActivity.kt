2510eace4f23ed6a07c42f64d1d394ac59af086b
parneet-guraya <gurayaparneet@gmail.com>
2024-03-08 02:04:55 +0530

    pass screens directly in Navhost
    Signed-off-by: parneet-guraya <gurayaparneet@gmail.com>

--- NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -19,7 +19,6 @@ package com.example.compose.rally
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
@@ -28,10 +27,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.compose.rally.ui.accounts.AccountsScreen
+import com.example.compose.rally.ui.bills.BillsScreen
 import com.example.compose.rally.ui.components.RallyTabRow
+import com.example.compose.rally.ui.overview.OverviewScreen
 import com.example.compose.rally.ui.theme.RallyTheme
 
 /**
@@ -51,27 +56,45 @@ class RallyActivity : ComponentActivity() {
 fun RallyApp() {
     RallyTheme {
         val navController = rememberNavController()
-        var currentScreen: RallyDestination by remember { mutableStateOf(Overview) }
+        val currentScreen by navController.currentBackStackEntryAsState()
         Scaffold(
             topBar = {
                 RallyTabRow(
                     allScreens = rallyTabRowScreens,
-                    onTabSelected = { screen -> currentScreen = screen },
-                    currentScreen = currentScreen
+                    onTabSelected = { screen ->
+                        navController.navigateSingleTopTo(screen.route)
+                    },
+                    currentScreen = rallyTabRowScreens.find { it.route == currentScreen?.destination?.route } ?: Overview
                 )
             }
         ) { innerPadding ->
-            NavHost(navController = navController, startDestination = Overview.route ,Modifier.padding(innerPadding)) {
-                composable(Overview.route){
-                    Overview.screen
+            NavHost(
+                navController = navController,
+                startDestination = Overview.route,
+                Modifier.padding(innerPadding)
+            ) {
+                composable(Overview.route) {
+                    OverviewScreen()
                 }
-                composable(Accounts.route){
-                    Accounts.screen
+                composable(Accounts.route) {
+                    AccountsScreen()
                 }
-                composable(Bills.route){
-                    Bills.screen
+                composable(Bills.route) {
+                    //TODO: If I omit () these at the end of screen will it now invoke the screen composable that's why I can't
+                    // see composable when not using (eg) Bills.screen
+                    BillsScreen()
                 }
             }
         }
+    }
+}
+
+fun NavHostController.navigateSingleTopTo(route: String) {
+    this.navigate(route) {
+        popUpTo(this@navigateSingleTopTo.graph.findStartDestination().id){
+            saveState = true
+        }
+        restoreState = true
+        launchSingleTop = true
     }
 }
